3b55d0e065c74447613ccacf3b89f5e69df30cdf
ObayIsm <obay.engineer@gmail.com>
2024-03-26 23:44:32 +0300

    Added x Unit Test Project

---
+++ SW.Payroll.Tests/SalarySplitProcessorTests.cs
@@ -0,0 +1,12 @@
+namespace SW.Payroll.Tests;
+
+public class SalarySplitProcessorTests
+{
+    //[Fact]
+    //public void Method_Senario_Outcome()
+    //{
+    //  1.Arrange
+    //  2.Act
+    //  3.Assert
+    //}
+}
