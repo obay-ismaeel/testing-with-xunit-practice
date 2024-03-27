cf0e5202bb5b3394162472073bfd5c5ecd46e305
ObayIsm <obay.engineer@gmail.com>
2024-03-27 17:15:39 +0300

    Added CalculateDangerPay() tests

--- SW.Payroll.Tests/SalarySplitProcessorTests.cs
+++ SW.Payroll.Tests/SalarySplitProcessorTests.cs
@@ -1,15 +1,20 @@
+using Moq;
+
 namespace SW.Payroll.Tests;
 
 public class SalarySplitProcessorTests
 {
-    //[Fact]
-    //public void Method_Senario_Outcome()
-    //{
-    //  1.Arrange
-    //  2.Act
-    //  3.Assert
-    //}
+    /**
+     *[Fact]
+     *public void Method_Senario_Outcome()
+     *{
+     *  1.Arrange
+     *  2.Act
+     *  3.Assert
+     *}
+     **/
 
+    // CalculateBasicSalary()
     [Fact]
     public void CalculateBasicSalary_ForEmployeeIsNull_ThrowArgumentNullException()
     {
@@ -37,6 +42,7 @@ public class SalarySplitProcessorTests
         Assert.Equal(expected, actual);
     }
 
+    // CalculateTransportationAllowece()
     [Fact]
     public void CalculateTransportationAllowece_ForEmployeeIsNull_ThrowArgumentNullException()
     {
@@ -91,4 +97,62 @@ public class SalarySplitProcessorTests
 
         Assert.Equal(expected, actual);
     }
+
+
+    // CalculateDangerPay()
+    [Fact]
+    public void CalculateDangerPay_ForEmployeeIsNull_ThrowArgumentNullException()
+    {
+        Employee employee = null;
+
+        var zone = new ZoneService();
+        var processor = new SalarySlipProcessor(zone);
+
+        Func<Employee, decimal> func = (e) => processor.CalculateDangerPay(e);
+
+        Assert.Throws<ArgumentNullException>(() => func(employee));
+    }
+
+    [Fact]
+    public void CalculateDangerPay_ForEmployeeIsDangerOn_ReturnsDangerPayAmount()
+    {
+        var employee = new Employee { IsDanger = true };
+
+        var zone = new ZoneService();
+        var processor = new SalarySlipProcessor(zone);
+
+        var actual = processor.CalculateDangerPay(employee);
+        var expected = Constants.DangerPayAmount;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void CalculateDangerPay_ForEmployeeIsDangerOffAndInDangerZone_ReturnsDangerPayAmount()
+    {
+        var employee = new Employee { IsDanger = false, DutyStation = "Ukraine" };
+        var mock = new Mock<IZoneService>();
+        var setup = mock.Setup(z => z.IsDangerZone(employee.DutyStation)).Returns(true);
+
+        var processor = new SalarySlipProcessor(mock.Object);
+        var actual = processor.CalculateDangerPay(employee);
+        var expected = Constants.DangerPayAmount;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void CalculateDangerPay_ForEmployeeIsDangerOffAndInDangerZoneOff_ReturnsDangerPayAmount()
+    {
+        var employee = new Employee { IsDanger = false, DutyStation = "Germany" };
+        var mock = new Mock<IZoneService>();
+        var setup = mock.Setup(z => z.IsDangerZone(employee.DutyStation)).Returns(false);
+
+        var processor = new SalarySlipProcessor(mock.Object);
+        var actual = processor.CalculateDangerPay(employee);
+        var expected = 0m;
+
+        Assert.Equal(expected, actual);
+    }
+
 }
